2970fab2b5c3ba557ec0c0e1c25849238930b254
root <y.com>
2022-07-04 02:54:59 -0700

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints every letter of the alphabet
+ * Return: 0 (Success)
+ */
+
+char main(void)
+{
+	char a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		if (a != 101 && a != 113)
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
