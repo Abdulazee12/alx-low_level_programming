35c327d27a937c3974c8b81eedb71815e665e9e4
root <y.com>
2022-07-05 17:58:11 -0700

    edited 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * _islower - prints 1 or 0 depending on input
- * @c: first parameter
- *
- * Description: prints all lowercase letters
- * Return: Always(0).
+ * _islower - Program to check lowercase character
+ * @c: integer variable to be check
+ * Return: 1 if it is lowercase and 0 if it is not a lowercase
  */
 
 int _islower(int c)
