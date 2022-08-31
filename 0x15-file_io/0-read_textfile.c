9b6a9f77fbaf14bf08d16b9fa091d92ad0e1a650
root <y.com>
2022-08-31 07:48:20 -0700

    edited all errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,44 @@
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - function that reads a text file and
- * prints it to the POSIX standard output.
- * @filename: name of the file to be read
- * @letters: the numbers of letters it should read and print.
+ * read_textfile - reads a text file and prints it to the POXIS standard output
+ * @filename: name of the file to read
+ * @letters: number of letters it should read and print
  *
- * Return: the actual number of letters it could read and print
+ * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *print_field;
-	int file, read_file;
+	int fd;
+	ssize_t lenr, lenw;
+	char *buffer;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
-
-	/*Create the buffer print_field*/
-	print_field = malloc(letters * sizeof(char));
-	if (!print_field)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-	/*Open the file*/
-	file = open(filename, O_RDONLY);
-	if (file == -1)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
 		return (0);
-
-	/*Read the file and save in buffer*/
-	read_file = read(file, print_field, letters);
-	/*Write as standard output*/
-	write(STDOUT_FILENO, print_field, read_file);
-
-	close(file);
-	free(print_field);
-	return (read_file);
+	}
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenw = write(STDOUT_FILENO, buffer, lenr);
+	free(buffer);
+	if (lenr != lenw)
+		return (0);
+	return (lenw);
 }
