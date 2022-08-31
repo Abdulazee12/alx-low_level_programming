5a6270937398256eb453c32c2cb06ab7846c4cef
root <y.com>
2022-08-31 15:19:02 -0700

    edited

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,25 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int res;
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+	return (0);
+}
 /**
  * _strlen - finds the length of a string
  * @str: pointer to the string
