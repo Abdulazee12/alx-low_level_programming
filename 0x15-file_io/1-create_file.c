ddb184191be1207cf73bf1ee482e9d022fe4b5cb
root <y.com>
2022-09-01 16:17:10 -0700

    deleted

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,21 +7,6 @@
 #include <stdlib.h>
 
 /**
- * _strlen - finds the length of a string
- * @str: pointer to the string
- *
- * Return; length of the string
- */
-size_t _strlen(char *str)
-{
-	size_t i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
-
-/**
  * create_file - creates a file.
  * @filename: name of the file to create
  * @text_content: NULL terminated string to write to the file
